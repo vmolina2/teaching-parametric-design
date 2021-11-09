88d378cbaa0ada11ed635c42332e30a61a8619d2
vmolina2 <93215876+vmolina2@users.noreply.github.com>
2021-11-10 00:00:26 +0100

    Task-02 versuch

--- code/task-02.1/sketch.js
+++ code/task-02.1/sketch.js
@@ -7,30 +7,34 @@ function setup() {
 
 function draw() {
       noLoop();
-      background(150);
+      background(200);
       
-      stroke("black");
-      strokeWeight(10);
       noFill();
-        for(let x = 1; x < 5; x += 1) {
-        for(let y = 1; y < 5; y += 1) {
-        line(10, (sketchHeight/4) * y-10,
-             80, (sketchHeight/4)*y-(sketchHeight/4-10));
-        }
+      stroke("black");
+    
+      let radius = 200;
+       
+      let maxCount = 20;
+      for (let circleCount = 0; circleCount < maxCount; circleCount += 1) {
+        let theta = (Math.PI / 180) * (360 / maxCount * circleCount + 90);
+    
+        let x = radius / 2 * cos(theta);
+        let y = radius / 2 * sin(theta);
+    
+        circle(
+          x + sketchWidth / 2,
+          y + sketchHeight / 2,
+          radius
+        );
       }
-        for(let x = 1; x < 5; x += 1) {
-        for(let y = 1; y < 5; y += 1) {
-        line(320, (sketchHeight/4) * y-10,
-             390, (sketchHeight/4)*y-(sketchHeight/4-10));
-        }
-      }
-          strokeWeight(20);
-          circle(sketchHeight/2, sketchWidth/2, sketchHeight-220);
-          
-          strokeWeight(5);
-          circle(sketchHeight/2, sketchWidth/2, 30);
-          circle(sketchHeight/2, sketchWidth-30, 30);
-          circle(sketchHeight/2, 30, 30);
+      radius = 70
+      maxCount = 6;
+    for (let circleCount = 0; circleCount < maxCount; circleCount += 1) {
+    let theta = (Math.PI / 180) * (360 / maxCount * circleCount + 90);
+
+    let x = radius / 2 * cos(theta);
+    let y = radius / 2 * sin(theta);
     }
+}
 
       
