9d48a61410291b35f9ea1a239835b7396a5a05c1
vmolina2 <93215876+vmolina2@users.noreply.github.com>
2021-11-16 18:16:04 +0100

    test. 03

---
+++ code/task-03/sketch.js
@@ -0,0 +1,32 @@
+const sketchWidth = 400;
+const sketchHeight = 400;
+const numPoints = 30;
+const velocity = 2;
+const points = [];
+for (let p = 0; p < numPoints; p += 1) {
+  points.push([
+    Math.random() * sketchWidth, // x
+    Math.random() * sketchHeight, // y
+    Math.random() * velocity, // dx
+    Math.random() * velocity // dy
+  ]);
+}
+
+const numLoops = 20;
+const minOpacity = 200;
+function draw() {
+  noLoop();
+  for (let l = 0; l < numLoops; l += 1) {
+    noFill();
+    stroke(0,minOpacity/numLoops * l)
+    beginShape();
+    for (let p = 0; p < points.length; p += 1) {
+      curveVertex(
+        points[p][0] + l * points[p][2],
+        points[p][1] + l * points[p][3]
+      );
+    }
+    endShape();
+  }
+}
+      
